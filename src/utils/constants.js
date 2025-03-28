51a15fae46b6ac702cf4b05195d7c4e259207e88
akriti-gupta27 <akritigupta8596@gmail.com>
2025-03-28 14:29:21 +0530

    Register to TMDB API and make thr get api call episode 15

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,11 @@ export const LOGO = "https://assets.nflxext.com/ffe/siteui/vlv3/fbf440b2-24a0-49
 export const photoURL = "https://avatars.githubusercontent.com/u/177637451?v=4";
 
 export const bgURL = "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+
+export const API_OPTIONS ={
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZmI1OWViZjY5MTdmZjk3OWRmNWZlYWVlMDRkZGY1NCIsIm5iZiI6MTc0MzE1MDcxOS44MTksInN1YiI6IjY3ZTY1ZTdmM2U2NWM4ZWE4OGJhM2EwZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EDDC54s0JnRyFM6xJW85Yi89rqeWM1AvWhc_opKVPiY'
+    },
+}
