a6ccb73d608ccd007a4e0c53a713a5b344199748
akriti-gupta27 <akritigupta8596@gmail.com>
2025-03-28 14:40:12 +0530

    Creating a slice for the movies and storing the data in our store episode 15

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,13 +1,18 @@
 import React, { useEffect } from 'react'
 import Header from './Header'
 import { API_OPTIONS } from '../utils/constants'
+import { useDispatch } from 'react-redux'
+import {addNowPlayingMovie} from "../utils/moviesSlice"
 
 const Browse = () => {
+
+  const dispatch = useDispatch();
 
   const getNowPlayingMovie = async () => {
     const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS);
     const json = await data.json();
     console.log(json.results);
+    dispatch(addNowPlayingMovie(json.results));
   };
 
   useEffect(() => {
